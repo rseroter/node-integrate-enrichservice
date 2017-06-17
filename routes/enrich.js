e49fe1edf5bff8b43e2f2cb969ccbd16ddc4a404
Richard Seroter <richardseroter@Richards-MacBook-Pro-2.local>
2017-06-17 14:24:05 -0700

    Initial commit

---
+++ routes/enrich.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var supplierdb = 
+    [
+        {supplierid:'100', companyname:'Pivotal', country:'United States'},
+        {supplierid:'101', companyname:'Microsoft', country:'United States'},
+        {supplierid:'102', companyname:'Kovai Ltd', country:'England'}
+    ];
+
+
+/* POST enrichment request */
+router.post('/', function(req, res) {
+
+    var order = req.body;
+
+    //lookup customer and add to order
+    var supplier = supplierdb.find(c => c.supplierid === order.supplierid);
+    order.supplier = supplier;
+
+    console.log(order);
+
+    res.send(order);
+});
+
+module.exports = router;
